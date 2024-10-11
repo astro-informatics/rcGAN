459cc7318f32add21d68bc050ee8f863e89ac977
Jessica Whitney <jessica.whitney.22@ucl.ac.uk>
2024-10-11 12:40:15 +0100

    Removes unused util functions

--- mass_map_utils/scripts/ks_utils.py
+++ mass_map_utils/scripts/ks_utils.py
@@ -18,97 +18,6 @@ def backward_model(γ: np.ndarray, 𝒟: np.ndarray) -> np.ndarray:
     return np.fft.ifft2(𝓕𝜅)  # Perform 2D inverse FFT
 
 
-# def Gaussian_smoothing(kappa:np.ndarray,n:int,theta:float,epsilon=25) -> np.ndarray:
-#     """Applies Gaussian smoothing to a convergence map.
-
-#     This is done by taking Fourier transform of the convergence map, and a Gaussian,
-#     convolving them, and then applying an inverse Fourier transform to the result.
-
-#     Args:
-#         kappa (np.ndarray): Convergence map.
-#         n (int): The dimensions, in pixels, of a square map kappa, where n x n is the no. of pixels in kappa.
-#         theta (float): Opening angle in deg.
-#         epsilon (int): Smoothing scale.
-
-#     Returns:
-#         smoothed_kappa (np.ndarray): Returns a smoothed representation of the the convergence field.
-#     """
-#     kappa_f = np.fft.fft2(kappa) #Fourier transform of kappa
-#     kappa_f_shifted = np.fft.fftshift(kappa_f) #Changes the indexing of the Fourier coefficients
-
-#     Gaussian_filter = np.zeros((n,n))
-#     i = (epsilon*n)/(60*theta)
-#     sig_pix = i/(2*np.sqrt(2*np.log(2)))
-
-#     t = int(n/2)
-#     y = np.arange(-t,t)
-#     xx,yy = np.meshgrid(y,y)
-
-#     exponential = np.exp(-(xx**2 + yy**2)*2*(sig_pix*np.pi)**2)
-#     Gaussian_filter = exponential
-
-#     smoothed_kappa_f = np.fft.ifftshift(kappa_f_shifted*Gaussian_filter)
-#     smoothed_kappa = np.fft.ifft2(smoothed_kappa_f)
-#     return smoothed_kappa
-
-
-"""
-ks93 and ks93inv from lenspack (Austin Peel, Cosmostat).
-https://github.com/austinpeel/lenspack/blob/master/lenspack/image/inversion.py
-"""
-
-
-def ks93(g1, g2):
-    """Direct inversion of weak-lensing shear to convergence.
-
-    This function is an implementation of the Kaiser & Squires (1993) mass
-    mapping algorithm. Due to the mass sheet degeneracy, the convergence is
-    recovered only up to an overall additive constant. It is chosen here to
-    produce output maps of mean zero. The inversion is performed in Fourier
-    space for speed.
-
-    Parameters
-    ----------
-    g1, g2 : array_like
-        2D input arrays corresponding to the first and second (i.e., real and
-        imaginary) components of shear, binned spatially to a regular grid.
-
-    Returns
-    -------
-    kE, kB : tuple of numpy arrays
-        E-mode and B-mode maps of convergence.
-
-    Raises
-    ------
-    AssertionError
-        For input arrays of different sizes.
-    """
-    # Check consistency of input maps
-    assert g1.shape == g2.shape
-
-    # Compute Fourier space grids
-    (nx, ny) = g1.shape
-    k1, k2 = np.meshgrid(np.fft.fftfreq(ny), np.fft.fftfreq(nx))
-
-    # Compute Fourier transforms of g1 and g2
-    g1hat = np.fft.fft2(g1)
-    g2hat = np.fft.fft2(g2)
-
-    # Apply Fourier space inversion operator
-    p1 = k1 * k1 - k2 * k2
-    p2 = 2 * k1 * k2
-    k2 = k1 * k1 + k2 * k2
-    k2[0, 0] = 1  # avoid division by 0
-    kEhat = (p1 * g1hat + p2 * g2hat) / k2
-    kBhat = -(p2 * g1hat - p1 * g2hat) / k2
-
-    # Transform back to pixel space
-    kE = np.fft.ifft2(kEhat).real
-    kB = np.fft.ifft2(kBhat).real
-
-    return kE, kB
-
-
 # def rmse(a: torch.float64, b: torch.float64, mask: np.ndarray = None) -> float:
 #     """
 #     args:
@@ -176,7 +85,8 @@ def ks93(g1, g2):
 #     noise = torch.mean((a - b) ** 2)
 #     return 10 * torch.log10(signal / noise)
 
-#NUMPY VERSIONS
+# NUMPY VERSIONS
+
 
 def rmse(a: np.ndarray, b: np.ndarray, mask: bool) -> float:
     """
@@ -187,6 +97,9 @@ def rmse(a: np.ndarray, b: np.ndarray, mask: bool) -> float:
     returns:
         rmse (float): root mean squared error
     """
+    if a.shape != b.shape:
+        raise ValueError("Shapes of a and b do not match.")
+
     a = a[mask == 1]
     b = b[mask == 1]
     return np.sqrt(np.mean(np.square(a - b)))
@@ -201,6 +114,9 @@ def pearsoncoeff(a: np.ndarray, b: np.ndarray, mask: bool) -> float:
     returns:
         pearson (float): Pearson correlation coefficient
     """
+    if a.shape != b.shape:
+        raise ValueError("Shapes of a and b do not match.")
+
     a = a[mask == 1]
     b = b[mask == 1]
     a -= np.mean(a)
@@ -219,6 +135,9 @@ def psnr(a: np.ndarray, b: np.ndarray, mask: bool) -> float:
     returns:
         psnr (float): peak signal-to-noise ratio
     """
+    if a.shape != b.shape:
+        raise ValueError("Shapes of a and b do not match.")
+
     a = a[mask == 1]
     b = b[mask == 1]
     mse = np.mean((a - b) ** 2)
@@ -235,6 +154,9 @@ def snr(a: np.ndarray, b: np.ndarray, mask: bool) -> float:
     returns:
         snr (float): signal-to-noise ratio
     """
+    if a.shape != b.shape:
+        raise ValueError("Shapes of a and b do not match.")
+
     a = a[mask == 1]
     b = b[mask == 1]
     signal = np.mean(a**2)
