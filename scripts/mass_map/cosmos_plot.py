db5aa1724b2db2ee163f83faadf1e7bfe5ecdf4e
Jess Whitney <jess.j.whitney@gmail.com>
2025-04-23 22:10:51 +0100

    Updates scripts to handle real dataloader

--- scripts/mass_map/cosmos_plot.py
+++ scripts/mass_map/cosmos_plot.py
@@ -6,12 +6,9 @@ import numpy as np
 import sys
 sys.path.append("/home/jjwhit/rcGAN/")
 from data.lightning.MassMappingDataModule import MMDataModule
-from data.lightning.MassMappingDataModule import MMDataTransform 
 from utils.parse_args import create_arg_parser
 from pytorch_lightning import seed_everything
 from models.lightning.mmGAN import mmGAN
-from utils.mri.math import tensor_to_complex_np
-from utils.mri import transforms
 from scipy import ndimage
 
 
@@ -25,88 +22,74 @@ if __name__ == "__main__":
     seed_everything(1, workers=True)
 
     config_path = args.config
-
     with open(config_path, "r") as f:
         cfg = yaml.load(f, Loader=yaml.FullLoader)
         cfg = json.loads(json.dumps(cfg), object_hook=load_object)
 
     dm = MMDataModule(cfg)
-    fig_count = 1
     dm.setup()
-    test_loader = dm.test_dataloader()
+    fig_count = 1
+    real_loader = dm.real_dataloader()
 
     with torch.no_grad():
         mmGAN_model = mmGAN.load_from_checkpoint(
-            # checkpoint_path=cfg.checkpoint_dir + args.exp_name + "/checkpoint_best.ckpt"
-            checkpoint_path=cfg.checkpoint_dir
-            + args.exp_name
-            + "/checkpoint-epoch=93.ckpt"
+            checkpoint_path=cfg.checkpoint_dir + args.exp_name + "/checkpoint-epoch=93.ckpt"
         )
-
         mmGAN_model.cuda()
-
         mmGAN_model.eval()
 
-        cosmos_map = np.load('/home/jjwhit/rcGAN/mass_map_utils/cosmos/cosmos_shear_cropped.npy', allow_pickle=True)
-        ks = MMDataTransform.backward_model(cosmos_map, MMDataTransform.compute_fourier_kernel(300))
-        pt_ks = transforms.to_tensor(ks)
-        pt_ks = pt_ks.permute(2,0,1)
-        cosmos_map = transforms.to_tensor(cosmos_map)
-        cosmos_map = cosmos_map.permute(2,0,1)
-        normalized_gamma, mean, std = transforms.normalise_complex(cosmos_map)
-        normalized_ks, mean_ks, std_ks = transforms.normalise_complex(pt_ks)
-        normalized_ks = transforms.normalize(pt_ks, mean_ks, std_ks)
-        normalized_gamma = torch.cat([normalized_gamma, normalized_ks], dim=0).unsqueeze(0).float()
-        batch_size = 9
-        normalized_gamma = normalized_gamma.repeat(batch_size, 1, 1, 1)
-        gens_mmGAN = torch.zeros(
-            size=(batch_size, cfg.num_z_test, cfg.im_size, cfg.im_size)
-        ).cuda()
+        for i, data in enumerate(real_loader):
+            y, x, mean, std = data
+            y = y.cuda()
 
-        for z in range(cfg.num_z_test):
-            gens_mmGAN[:, z, :, :] = mmGAN_model.reformat(mmGAN_model.forward(normalized_gamma.cuda())).squeeze(dim=-1)
+            gens_mmGAN = torch.zeros(
+                size=(y.size(0), cfg.num_z_test, cfg.im_size, cfg.im_size)
+            ).cuda()  # y.size(0) = batch size
 
-        avg_mmGAN = torch.mean(gens_mmGAN, dim=1)
-        for j in range(batch_size):
-            np_avgs = {
-                "mmGAN": None,
-            }
-
-            np_samps = {
-                "mmGAN": [],
-            }
-
-            np_stds = {
-                "mmGAN": None,
-            }
-            kappa_mean = cfg.kappa_mean
-            kappa_std = cfg.kappa_std
-            np_avgs["mmGAN"] = ndimage.rotate(
-                (avg_mmGAN[j] * kappa_std + kappa_mean).cpu().numpy(), 180
-            )
             for z in range(cfg.num_z_test):
-                np_samps["mmGAN"].append(
-                    ndimage.rotate(
-                        (gens_mmGAN[j, z] * kappa_std + kappa_mean).cpu().numpy(),
-                        180,
+                gens_mmGAN[:, z, :, :] = mmGAN_model.reformat(mmGAN_model.forward(y)).squeeze(-1)
+
+            avg_mmGAN = torch.mean(gens_mmGAN, dim=1)
+            for j in range(y.size(0)):
+                np_avgs = {
+                    "mmGAN": None,
+                }
+
+                np_samps = {
+                    "mmGAN": [],
+                }
+
+                np_stds = {
+                    "mmGAN": None,
+                }
+                kappa_mean = cfg.kappa_mean
+                kappa_std = cfg.kappa_std
+                np_avgs["mmGAN"] = ndimage.rotate(
+                    (avg_mmGAN[j] * kappa_std + kappa_mean).squeeze().cpu().numpy(), 180
+                )
+                for z in range(cfg.num_z_test):
+                    np_samps["mmGAN"].append(
+                        ndimage.rotate(
+                            (gens_mmGAN[j, z] * kappa_std + kappa_mean).squeeze().cpu().numpy(),
+                            180,
+                        )
                     )
+
+                np_stds["mmGAN"] = np.std(np.stack(np_samps["mmGAN"]), axis=0)
+
+                np.save(
+                    f"/share/gpu0/jjwhit/samples/real_output/cosmos/np_avgs_cos_{fig_count}.npy",
+                    np_avgs["mmGAN"],
+                )
+                np.save(
+                    f"/share/gpu0/jjwhit/samples/real_output/cosmos/np_stds_cos_{fig_count}.npy",
+                    np_stds["mmGAN"],
+                )
+                np.save(
+                    f"/share/gpu0/jjwhit/samples/real_output/cosmos/np_samps_cos_{fig_count}.npy",
+                    np_samps["mmGAN"],
                 )
 
-            np_stds["mmGAN"] = np.std(np.stack(np_samps["mmGAN"]), axis=0)
-
-        np.save(
-            f"/share/gpu0/jjwhit/samples/real_output/cosmos/np_avgs_cos.npy",
-            np_avgs["mmGAN"],
-        )
-        np.save(
-            f"/share/gpu0/jjwhit/samples/real_output/cosmos/np_stds_cos.npy",
-            np_stds["mmGAN"],
-        )
-        np.save(
-            f"/share/gpu0/jjwhit/samples/real_output/cosmos/np_samps_cos.npy",
-            np_samps["mmGAN"],
-        )
-
-        if fig_count == args.num_figs:
-            sys.exit()
-        fig_count += 1
+                if fig_count == args.num_figs:
+                    sys.exit()
+                fig_count += 1
